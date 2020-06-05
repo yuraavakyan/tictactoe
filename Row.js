23c2d44657f851ad604129db15597e4c0f5e02b0
yuraavakyan <62063534+yuraavakyan@users.noreply.github.com>
2020-06-05 13:53:28 +0300

    Update Row.js

--- Row.js
+++ Row.js
@@ -9,7 +9,6 @@ let count = 0;
 const Row = (props) => {
     const [state, setState] = useState(Array(props.cols).fill(null));
 
-
     const clickHandler = (event) => {
 
         const i = event.target.id;
@@ -22,89 +21,128 @@ const Row = (props) => {
                 cells[i] = 'X';
                 arrX.push({ parent: parent, index: i });
                 checkWin(arrX);
-                console.log(arrX);
-
-
-
-
+                //console.log(arrX);
             } else {
                 cells[i] = 'O';
                 arrO.push({ parent: parent, index: i });
                 checkWin(arrO);
-                console.log(arrO);
+                // console.log(arrO);
             }
             count += 1;
             setState(cells);
-
-
-
         }
         else
             alert('the field is not empty...');
-
-
     }
 
     const checkWin = (array) => {
-
-
+        let count = 0;
 
         const horizontalCheck = () => {
-            const arr = [];
+            const arrObjects = [];
+            const arrParents = [];
+
             for (let i = 0; i < array.length; i++) {
                 for (let j = 0; j < array.length; j++) {
                     if (array[i].parent === array[j].parent && Math.abs(array[i].index - array[j].index) === 1) {
-                        if (!arr.includes(array[i])) arr.push(array[i]);
-                        if (!arr.includes(array[j])) arr.push(array[j]);
-                        if (arr.length === 5) return true;
+                        if (!arrObjects.includes(array[i])) {
+                            arrObjects.push(array[i]);
+                            arrParents.push(array[i].parent);
+                        }
+                        if (!arrObjects.includes(array[j])) {
+                            arrObjects.push(array[j]);
+                            arrParents.push(array[j].parent);
+                        }
+                        arrParents.sort((a, b) => { return a - b });
+                        for (let i = 0; i < arrParents.length; i++) {
+                            if (arrParents[i] === arrParents[i + 1]) {
+                                count++;
+                                if (count === 4) return true;
+                            }
+                            else count = 0;
+                        }
                     }
-
                 }
             }
-            console.log('horizontal: ' + arr);
-
             return false;
+
         }
 
         const verticalCheck = () => {
-            const arr = [];
+            const arrObjects = [];
+            const arrIndexes = [];
+
             for (let i = 0; i < array.length; i++) {
                 for (let j = 0; j < array.length; j++) {
                     if (array[i].index === array[j].index && Math.abs(array[i].parent - array[j].parent) === 1) {
-                        if (!arr.includes(array[i])) arr.push(array[i]);
-                        if (!arr.includes(array[j])) arr.push(array[j]);
-                        if (arr.length === 5) return true;
-                    }
+                        if (!arrObjects.includes(array[i])) {
+                            arrObjects.push(array[i]);
+                            arrIndexes.push(array[i].index);
+                        }
+                        if (!arrObjects.includes(array[j])) {
+                            arrObjects.push(array[j]);
+                            arrIndexes.push(array[j].index);
+                        }
+                        arrIndexes.sort((a, b) => { return a - b });
+                        for (let i = 0; i < arrIndexes.length; i++) {
+                            if (arrIndexes[i] === arrIndexes[i + 1]) {
+                                count++;
 
+
+                                if (count === 4) return true;
+                            }
+                            else count = 0;
+                        }
+
+                    }
                 }
             }
-            console.log('vertical: ' + arr);
             return false;
+
         }
 
         const diagonalCheck = () => {
-            const arr = [];
+            const arrObjects = [];
+            const arrParents = [];
+            const arrIndexes = [];
+
             for (let i = 0; i < array.length; i++) {
                 for (let j = 0; j < array.length; j++) {
                     if (Math.abs(array[i].parent - array[j].parent) === 1 && Math.abs(array[i].index - array[j].index) === 1) {
-                        if (!arr.includes(array[i])) arr.push(array[i]);
-                        if (!arr.includes(array[j])) arr.push(array[j]);
-                        if (arr.length === 5) return true;
+                        if (!arrObjects.includes(array[i])) {
+                            arrObjects.push(array[i]);
+                            arrParents.push(array[i].parent);
+                            arrIndexes.push(array[i].index);
+                        }
+                        if (!arrObjects.includes(array[j])) {
+                            arrObjects.push(array[j]);
+                            arrParents.push(array[j].parent);
+                            arrIndexes.push(array[j].index);
+                        }
+                        arrParents.sort((a, b) => { return a - b });
+                        arrIndexes.sort((a, b) => { return a - b });
+                        // console.log('parents: ' + arrParents);
+                        // console.log('indexes: ' + arrIndexes);
+                        for (let i = 0; i < arrIndexes.length; i++) {
+                            if (Math.abs(arrParents[i] - arrParents[i + 1]) === 1 && Math.abs(arrIndexes[i] - arrIndexes[i + 1]) === 1) {
+                                count++;
+                                //  console.log(count);
+                                if (count === 20) return true;
+                            }
+                            else if (Math.abs(arrParents[i] - arrParents[i + 1]) === 1 || Math.abs(arrIndexes[i] - arrIndexes[i + 1]) === 1) {
+                                arrParents.splice(i, 1);
+                                arrIndexes.splice(i, 1);
+                                count = 0;
+                            }
+                        }
                     }
-
                 }
             }
-            console.log('diagonal: ' + arr);
             return false;
         }
-
-
-        if (horizontalCheck()) alert(`${array[0]} wins`);
-        if (verticalCheck()) alert('vertical wins');
-        if (diagonalCheck()) alert('diagonal wins');
-
-
-
+        if (horizontalCheck()) alert(`${array[0].value} horizontal wins`);
+        if (verticalCheck()) alert(`${array[0].value} vertical wins`);
+        if (diagonalCheck()) alert(`${array[0].value} diagonal wins`);
     }
 
     const cols = props.cols;
@@ -116,15 +154,10 @@ const Row = (props) => {
         }
         return arr;
     }
-
-
     return (
         <div className='board-row' id={props.id}>
             {renderRow(cols)}
         </div>
     );
 }
-
-
-
 export default Row;
